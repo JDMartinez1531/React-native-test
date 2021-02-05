60b1744da8092843852222832690d30224081beb
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2021-02-05 13:15:00 -0600

    finished first tutorial

--- App.js
+++ App.js
@@ -1,52 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen"
+import ViewImageScreen from "./app/screens/ViewImageScreen"
 
 export default function App() {
 
   return (
-    <View style={{
-      backgroundColor: '#fff',
-      flex: 1,
-      alignItems: 'center'
-    }}>
-    <Text style={{marginTop: "20%"}}>Hellow World!</Text>
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      
-<View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        height: 100,
-      }}
-/>
-<View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-      }}
-/>
-<View style={{
-        backgroundColor: "purple",
-        width: 100,
-        height: 100,
-      }}
-/>
-    </View>
-    </View>
+    <ViewImageScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
